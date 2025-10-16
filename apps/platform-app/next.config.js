d31790f3e5ff7682822662057b26cb61c686cb10
regulocamba <cambaconsulting@gmail.com>
2025-10-16 13:06:12 -0400

    fix(vercel): final vercel configuration with turborepo and packageManager

--- apps/platform-app/next.config.js
+++ apps/platform-app/next.config.js
@@ -2,14 +2,11 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Esta configuración se aplica solo durante la compilación del lado del servidor
-    if (isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@react-native-async-storage/async-storage': path.resolve(__dirname, '__mocks__/react-native-async-storage.js'),
-      };
-    }
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@react-native-async-storage/async-storage': path.resolve(__dirname, '__mocks__/react-native-async-storage.js'),
+    };
     return config;
   },
 };
